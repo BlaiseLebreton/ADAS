6e9aa9977e4f6ead4d5e2b7b1f26c730eb695ab4
BlaiseLebreton <blaise.lebreton@sfr.fr>
2020-10-25 10:34:32 +0100

    Indent

--- cmd_par_raspberry.ino
+++ cmd_par_raspberry.ino
@@ -24,7 +24,7 @@ servo ServoPwr;
 cmd commande;
 
 void setup() {
-  
+
   Serial.begin(57600); // opens serial port, sets data rate to 57600 baud
   ServoDir.s.attach(2);// attacher le varaible de l'angle du servo-moteur à un pin
   ServoPwr.s.attach(3);
@@ -45,42 +45,42 @@ void loop() {
     }
     index = 0;
     int r_cmd = min(max(-90,  inData[0]), 90);
-    int r_pwr = min(max(1300, inData[1]), 1800);  
+    int r_pwr = min(max(1300, inData[1]), 1800);
     //ServoDir.ch = pulseIn(4,HIGH);
     //ServoPwr.ch = pulseIn(5,HIGH);
 
-    
+
     ServoDir.s.write(ServoDir.k * r_cmd + ServoDir.off + 90);
     ServoPwr.s.writeMicroseconds(r_pwr);
 
   }
-   
-//  ServoDir.s.write(ServoDir.k * -90 + ServoDir.off + 90); // On envoi la commande de l'angle au servo-moteur
-//  delay(1500);
-//  ServoDir.s.write(ServoDir.k * 90 + ServoDir.off + 90); // On envoi la commande de l'angle au servo-moteur
-//  delay(1500);
-//  ServoDir.s.write(ServoDir.k * 0 + ServoDir.off + 90); // On envoi la commande de l'angle au servo-moteur
-//  delay(100);
-//
-//  Serial.println("1500");
-//  ServoPwr.s.writeMicroseconds(1500); 
-//  delay(2500);
-//  Serial.println("1300");
-//  ServoPwr.s.writeMicroseconds(1300); //arriere
-//  delay(2500);
-//  Serial.println("1500");
-//  ServoPwr.s.writeMicroseconds(1500); 
-//  delay(2500);
-//  Serial.println("1700");
-//  ServoPwr.s.writeMicroseconds(1700); //avant
-//  delay(2500);
-//
-//  ServoDir.ch = pulseIn(4,HIGH);
-//  ServoPwr.ch = pulseIn(5,HIGH);
-//
-//  Serial.print(ServoDir.ch);
-//  Serial.print(",");
-//  Serial.println(ServoPwr.ch);
-//
-//  delay(100);
+
+  //  ServoDir.s.write(ServoDir.k * -90 + ServoDir.off + 90); // On envoi la commande de l'angle au servo-moteur
+  //  delay(1500);
+  //  ServoDir.s.write(ServoDir.k * 90 + ServoDir.off + 90); // On envoi la commande de l'angle au servo-moteur
+  //  delay(1500);
+  //  ServoDir.s.write(ServoDir.k * 0 + ServoDir.off + 90); // On envoi la commande de l'angle au servo-moteur
+  //  delay(100);
+  //
+  //  Serial.println("1500");
+  //  ServoPwr.s.writeMicroseconds(1500);
+  //  delay(2500);
+  //  Serial.println("1300");
+  //  ServoPwr.s.writeMicroseconds(1300); //arriere
+  //  delay(2500);
+  //  Serial.println("1500");
+  //  ServoPwr.s.writeMicroseconds(1500);
+  //  delay(2500);
+  //  Serial.println("1700");
+  //  ServoPwr.s.writeMicroseconds(1700); //avant
+  //  delay(2500);
+  //
+  //  ServoDir.ch = pulseIn(4,HIGH);
+  //  ServoPwr.ch = pulseIn(5,HIGH);
+  //
+  //  Serial.print(ServoDir.ch);
+  //  Serial.print(",");
+  //  Serial.println(ServoPwr.ch);
+  //
+  //  delay(100);
 }
