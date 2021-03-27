458737c1a23c7fd775f6789ee52b66a8724388d1
BlaiseLebreton <blaise.lebreton@sfr.fr>
2021-03-27 14:59:09 +0100

    Remove comment

--- raspberry/liaison.cpp
+++ raspberry/liaison.cpp
@@ -8,12 +8,12 @@
 #define DEBUG 0
 #define BUF_SIZE 128
 
-// int cport_nr=25; /* /dev/ttyACM1 */
 int cport_nr=24; /* /dev/ttyACM0 */
+char str_send[BUF_SIZE];
 
 int Liaison_Initialize() {
-  int bdrate=230400; /* 230400 baud */
-  char mode[]={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
+  int bdrate=230400;
+  char mode[]={'8','N','1',0};
   if(RS232_OpenComport(cport_nr, bdrate, mode, 0))
   {
     printf("Can not open comport\n");
@@ -22,23 +22,11 @@ int Liaison_Initialize() {
   return 0;
 }
 
-char str_send[BUF_SIZE];
-unsigned char str_recv[BUF_SIZE];
 int Liaison_SendData(int cmd, int pwr) {
   sprintf(str_send, "%d_%d", cmd, pwr);
-
-  // if (strcmp(str_send,(char*)str_recv) != 0) { // Si commande différente ou dernière commande mal transmise (str_recv erronee)
-    int n;
-    RS232_cputs(cport_nr, str_send);
-    if (DEBUG > 1)
-      printf("Sent: '%s'\n", str_send);
-    // do{
-    //   n = RS232_PollComport(cport_nr, str_recv, (int)BUF_SIZE);
-    //   str_recv[n] = 0;
-    //   if (n > 0 && DEBUG > 1)
-    //     printf("Recv: '%s'\n\n", str_recv);
-    //   usleep(100);  /* waits for reply 100ms */
-    // }while(n == 0);
-  // }
+  int n;
+  RS232_cputs(cport_nr, str_send);
+  if (DEBUG > 1)
+  printf("Sent: '%s'\n", str_send);
   return 0;
 }
